42a25d497494a067db67347b3ea7067f58cf52ff
noahlz <nzucker@gmail.com>
2013-06-20 23:56:31 -0400

    deleted unused code

--- src/main/clojure/stringly/service.clj
+++ src/main/clojure/stringly/service.clj
@@ -3,16 +3,12 @@
 
 (def ^:private stringly-fns
   (let [metadata (->> (ns-publics 'stringly.core) vals (map meta))]
-    (map (fn [{n :name [args] :arglists}] ;; only the first arg signature
+    (map (fn [{n :name [args] :arglists}] 
            {:name (str n) :arity (count args)})
          metadata)))
 
-(def ^:private stringly-fn-names
-  (keys (ns-publics 'stringly.core)))
-
 (defn list-operations [] stringly-fns)
 
-;; TODO using function metadata, use truthy to convert truthy to true and nil to false
 (defn invoke [f args]
   {:pre [(string? f) (vector? args) (every? string? args)]}
   (if-let [stringly-fn (ns-resolve 'stringly.core (symbol f))]
